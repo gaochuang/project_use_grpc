5dc33c1e0402e1ba0118e455a4f40d731a634b9c
gaochuang <ienjoyarmlinux@163.com>
2022-08-25 19:37:57 +0800

    Add client code

--- client.go
+++ client.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"user"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial(":8899", grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		conn.Close()
+	}()
+
+	client := user.NewUserServiceClient(conn)
+	req := &user.UserRequest{
+		Id: 1,
+	}
+
+	response, _ := client.GetUser(context.Background(), req)
+
+	resp, err := json.Marshal(response)
+
+	fmt.Printf("%s\n", resp)
+
+}
